f6e78b8c168e913bc4a019443826e2d75fca3aba
Mihkel Väli <mihkel.vali@hotmail.com>
2023-05-04 19:34:59 +0300

    js alused

---
+++ loeng08/script.js
@@ -0,0 +1,27 @@
+setTimeout(function() {
+  document.querySelector('#mingiDiv').innerHTML = 'tere tere';
+  document.querySelector('#mingiDiv').style.background = 'peachpuff';
+  document.querySelector('#mingiDiv').style.color = 'seagreen';
+  document.querySelector('#mingiDiv').style.fontWeight = 900;
+}, 1000);
+
+function generateRandom(min, max) {
+  let difference = max - min;
+  let rand = Math.random();
+  rand = Math.floor(rand * difference);
+  rand = rand + min;
+  return rand;
+}
+
+function muudaVarvi() {
+  let red = generateRandom(0, 255);
+  let green = generateRandom(0, 255);
+  let blue = generateRandom(0, 255);
+  console.log('rgb(' + red + ',' + green + ',' + blue + ')');
+  document.querySelector('#mingiDiv').style.background = 'rgb(' + red + ',' + green + ',' + blue + ')';
+}
+
+function kirjutaLogisse() {
+  let sisendiVaartus = document.querySelector('#sisend').value;
+  console.log(sisendiVaartus);
+}
